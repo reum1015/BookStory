4f4f14bfddb41834eea21e10bbfa81a63d215f27
yunsigbang <yunsigbang@192.168.0.4>
2016-11-29 23:39:06 +0900

    login and novelview  jsp part
    bookmark favorite model part

--- bookStory/src/study/jsp/bookstory/model/BookMark.java
+++ bookStory/src/study/jsp/bookstory/model/BookMark.java
@@ -3,18 +3,36 @@ package study.jsp.bookstory.model;
 public class BookMark {
 
 	private int id; 	// 북마크 번호
-
+	private int member_id;   // 회원번호
+	private int episode_id;    // 회차번호
+	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	public int getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(int member_id) {
+		this.member_id = member_id;
+	}
+	public int getEpisode_id() {
+		return episode_id;
+	}
+	public void setEpisode_id(int episode_id) {
+		this.episode_id = episode_id;
+	}
 	@Override
 	public String toString() {
-		return "BookMark [id=" + id + "]";
+		return "BookMark [id=" + id + ", member_id=" + member_id + ", episode_id=" + episode_id + "]";
 	}
+	
+	
+	
+	
+	
+
 	
 }
--- bookStory/src/study/jsp/bookstory/model/Favorite.java
+++ bookStory/src/study/jsp/bookstory/model/Favorite.java
@@ -3,18 +3,50 @@ package study.jsp.bookstory.model;
 public class Favorite {
 
 	private int id;		// 선호작품 번호
-
+	private int member_id;   //회원 번호
+	private int book_id;   // 책 번호
+	private String gender;   // 순위로 인한 성별
+	private String birthdate; // 순위로 인한 생년월일
+	
+	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	public int getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(int member_id) {
+		this.member_id = member_id;
+	}
+	public int getBook_id() {
+		return book_id;
+	}
+	public void setBook_id(int book_id) {
+		this.book_id = book_id;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
 	@Override
 	public String toString() {
-		return "Favorite [id=" + id + "]";
+		return "Favorite [id=" + id + ", member_id=" + member_id + ", book_id=" + book_id + ", gender=" + gender
+				+ ", birthdate=" + birthdate + "]";
 	}
+	
+	
+	
+	
 	
 }
