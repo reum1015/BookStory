3824a015be79049ff929ecd7431d093821001642
Administrator <Administrator@Sc-201609051410>
2016-11-29 10:30:16 +0900

    viewPage

---
+++ bookStory/src/study/jsp/bookstory/controller/book/ViewPage.java
@@ -0,0 +1,21 @@
+package study.jsp.bookstory.controller.book;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import study.jsp.helper.BaseController;
+
+public class ViewPage extends BaseController{
+
+	private static final long serialVersionUID = 113920651746454564L;
+
+	@Override
+	public String doRun(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
