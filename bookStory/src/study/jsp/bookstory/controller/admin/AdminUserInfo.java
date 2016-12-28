28b8a9ad0efe04ed749dc50971dd17843687dbb1
heo <eeee5058@gmail.com>
2016-12-29 01:41:03 +0900

    member_Service_delete_update_small_ok

--- bookStory/src/study/jsp/bookstory/controller/admin/AdminUserInfo.java
+++ bookStory/src/study/jsp/bookstory/controller/admin/AdminUserInfo.java
@@ -7,20 +7,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import study.jsp.bookstory.dao.MybatisConnectionFactory;
+import study.jsp.bookstory.service.ReportService;
+import study.jsp.bookstory.service.impl.ReportServiceImpl;
 import study.jsp.helper.BaseController;
+import study.jsp.helper.WebHelper;
 
 @WebServlet("/admin/admin_userInfo.do")
 public class AdminUserInfo extends BaseController{
 
-
-	/**
-	 * 
-	 */
+	Logger logger;
+	WebHelper web;
+	SqlSession sqlSession;
+	ReportService reportService;
+	
 	private static final long serialVersionUID = -8435966275594212677L;
 
 	@Override
 	public String doRun(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		logger = LogManager.getFormatterLogger(request.getRequestURL());
+		web = WebHelper.getInstance(request, response);
+		sqlSession = MybatisConnectionFactory.getSqlSession();
+		reportService = new ReportServiceImpl(sqlSession, logger);
+		
+		
+		
 		String view = "admin/admin_userInfo";
 		
 		return view;
